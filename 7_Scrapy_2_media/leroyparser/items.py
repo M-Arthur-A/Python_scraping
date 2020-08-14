a2f7a1c660f4e2d752583afeac8e00820637f34a
Arthur M <arthurandreevich@gmail.com>
2020-08-14 14:55:21 +0300

    hw7 is done

---
+++ 7_Scrapy_2_media/leroyparser/items.py
@@ -0,0 +1,28 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+from scrapy.loader.processors import MapCompose, TakeFirst, Compose
+
+
+def cleaner_txt(txt):
+    txt = txt.replace('\n', '')  # удаление переноса строки
+    txt = ''.join(txt.split('  ')).strip()  # удаление двойных пробелов
+    return txt
+
+
+def get_id(txt):
+    return txt.replace('Арт. ', '').strip()
+
+
+class LeroyparserItem(scrapy.Item):
+    _id = scrapy.Field(input_processor=MapCompose(get_id), output_processor=TakeFirst())
+    name = scrapy.Field(output_processor=TakeFirst())
+    photos = scrapy.Field()
+    params_name = scrapy.Field()
+    params_val = scrapy.Field(input_processor=MapCompose(cleaner_txt))
+    params = scrapy.Field()
+    href = scrapy.Field(output_processor=TakeFirst())
+    price = scrapy.Field(output_processor=TakeFirst())
